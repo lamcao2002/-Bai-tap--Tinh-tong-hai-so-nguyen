92babd12b9c3798ce5ef708c89851df3a2df1213
lamcao2002 <caovohoanglam2002@gmail.com>
2021-03-06 19:40:46 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+function tinhTong(){
+    let firstNumber = document.getElementById("firstNumber").value;
+    let secondNumber = document.getElementById("secondNumber").value;
+    let tong = parseInt(firstNumber) + parseInt(secondNumber);
+    document.getElementById("ketQua").innerHTML = tong;
+}
