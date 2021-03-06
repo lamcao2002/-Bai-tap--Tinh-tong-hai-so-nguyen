f3af558faa190d074aff71e527768adae0361cfa
lamcao2002 <caovohoanglam2002@gmail.com>
2021-03-06 19:50:52 +0700

    format code

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-function tinhTong(){
+function tinhTong() {
     let firstNumber = document.getElementById("firstNumber").value;
     let secondNumber = document.getElementById("secondNumber").value;
     let tong = parseInt(firstNumber) + parseInt(secondNumber);
-    document.getElementById("ketQua").innerHTML = tong;
+    document.getElementById("ketQua").innerHTML = "Tổng hai số là: " + tong;
 }
